88d88574d35903b2e379ad822085ff446f95b771
OdeliaFink <odelia.finkel@gmail.com>
2021-02-10 18:54:36 +0000

    refactored and exported connect function

--- play.js
+++ play.js
@@ -1,19 +1,3 @@
-const net = require('net');
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: '174.89.248.118',
-    port: 50541
-  });
-
-  conn.setEncoding('utf8');
-
-  conn.on('data', function(data) {
-    console.log("you ded cuz you idled")
-  })
-
-  return conn;
-}
-
-console.log('Connecting...');
+const { connect } = require('./client');
+console.log('Connecting ...');
 connect();
