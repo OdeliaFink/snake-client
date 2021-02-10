9358c44e195da1815c36bc7e5033c7242ecfac0f
OdeliaFink <odelia.finkel@gmail.com>
2021-02-10 20:28:47 +0000

    added direction input for snake

--- input.js
+++ input.js
@@ -1,18 +1,36 @@
-const handleUserInput = function(data) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
+let connection; 
 
-const setupInput = function() {
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-
+  
   stdin.on('data', handleUserInput)
   return stdin;
 }
 
+const handleUserInput = function(data) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+  if (data === 'w') {
+    connection.write('Move: up');
+   }
+   if (data === 's') {
+    connection.write('Move: down');
+   }
+   if (data === 'a') {
+    connection.write('Move: left');
+   }
+   if (data === 'd') {
+    connection.write('Move: right');
+   }
+   if (data === 't') {
+     connection.write('Say: Heyy!');
+    }
+}
 
 module.exports = { setupInput };
