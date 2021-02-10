0187ab142767bd81862fddc99c98e64c74dc6c12
OdeliaFink <odelia.finkel@gmail.com>
2021-02-10 20:04:13 +0000

    added exported function

--- play.js
+++ play.js
@@ -1,23 +1,7 @@
-const { stdin } = require('process');
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
-
-const handleUserInput = function(data) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  stdin.on('data', handleUserInput)
-  return stdin;
-}
-
 
 setupInput();
